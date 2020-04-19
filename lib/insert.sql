e5297afccf3fd8c83ff35a94b4266ff85e1c4eb2
Diane Crame <dcrame@icloud.com>
2020-04-19 21:25:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,7 @@
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "Meow", "Cats", 1000, 42020, 42120);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (2, "Woof", "Dogs", 2000, 52020, 62120);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (3, "Moo", "Cows", 32000, 61321, 72121);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (4, "Quack", "Ducks", 15000, 42020, 42120);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (5, "Baa", "Sheep", 20000, 42020, 42520);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (6, "Eee", "Dolphins", 30000, 81520, 92120);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (7, "Bzz", "Bees", 40000, 112020, 112120);
