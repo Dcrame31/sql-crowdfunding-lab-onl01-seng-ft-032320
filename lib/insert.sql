a3556a8270775fb281fa57c4d472d08719bac948
Diane Crame <dcrame@icloud.com>
2020-04-19 21:28:13 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,12 @@
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "Meow", "Cats", 1000, 42020, 42120);
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (2, "Woof", "Dogs", 2000, 52020, 62120);
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (3, "Moo", "Cows", 32000, 61321, 72121);
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (4, "Quack", "Ducks", 15000, 42020, 42120);
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (5, "Baa", "Sheep", 20000, 42020, 42520);
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (6, "Eee", "Dolphins", 30000, 81520, 92120);
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (7, "Bzz", "Bees", 40000, 112020, 112120);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES ("Meow", "Cats", 1000, 42020, 42120);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES ("Woof", "Dogs", 2000, 52020, 62120);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES ("Moo", "Cows", 32000, 61321, 72121);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES ("Quack", "Ducks", 15000, 42020, 42120);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES ("Baa", "Sheep", 20000, 42020, 42520);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES ("Eee", "Dolphins", 30000, 81520, 92120);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES ("Bzz", "Bees", 40000, 112020, 112120);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES ("Heehaw", "Donkeys", 134030, 42020, 42120);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES ("Woo!", "Elephants", 100000, 52020, 62120);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES ("Whoopee!", "Rhinos", 1299000, 42020, 42120);
+
+
