2e64fb68ca5f7e0ff9bafca699c5e7cc556a18e7
Diane Crame <dcrame@icloud.com>
2020-04-19 22:21:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,6 +27,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
    FROM Projects
    INNER JOIN pledges
    ON Projects.id = Pledges.project_id
+   HAVING SUM(pledges.amount) > Projects.funding_goal
    GROUP BY(title);"
 end
 
