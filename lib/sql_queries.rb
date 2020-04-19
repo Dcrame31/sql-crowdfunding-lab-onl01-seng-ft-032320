8cc849f4fdd8c11b0213d01329ef81c0877045f2
Diane Crame <dcrame@icloud.com>
2020-04-19 21:46:54 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   "SELECT Projects.title, Pledges.amount
   FROM projects
   INNER JOIN pledges
-  ON 
+  ON Projects.id = Pledges.
   ORDER BY title"
 end
 
